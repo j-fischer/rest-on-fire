46412f5ba79ca6a66ad3ce37c2ef73f41205109b
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-04-27 17:33:07 -0600

    Added Javadoc comments to FirebaseRestReference interface.

--- src/main/java/org/restonfire/FirebaseRestReference.java
+++ src/main/java/org/restonfire/FirebaseRestReference.java
@@ -5,20 +5,116 @@ import org.jdeferred.Promise;
 import org.restonfire.exceptions.FirebaseRuntimeException;
 
 /**
- * Created by jfischer on 2016-04-07.
+ * A {@link FirebaseRestReference} represents a specific location within a Firebase namespace and allows
+ * for operations to be executed on this location using Firebase's REST API.<br>
+ * <br>
+ * This interface is a mixture of Firebase's Java and Javascript API, using similar syntax as the Java (Android)
+ * API but returning a {@link Promise} to allow for a more functional implementation of asynchronous requests.
+ *
+ * @see <a href="https://github.com/jdeferred/jdeferred">JDeferred Library</a>
  */
 public interface FirebaseRestReference {
 
+  /**
+   * Returns the fully qualified URL for this FirebaseRestReference instance.
+   *
+   * @return The absolute URL of the current reference as a String.
+   */
   String getReferenceUrl();
 
+  /**
+   * Retrieves the value for this reference URL from Firebase.<br>
+   * <br>
+   * The promise returned will be rejected with the following two exceptions:<br>
+   * <pre>
+   * org.restonfire.exceptions.FirebaseAccessException: A {@link FirebaseRuntimeException} in the case that access to the data for this reference was denied.
+   * org.restonfire.exceptions.FirebaseRestException:   A {@link FirebaseRuntimeException} in the case that an unexpected status code was returned or the deserialization of the response into the type parameter fails.
+   * </pre>
+   * @param clazz The {@link Class} type for the POJO to be created for the data returned by the request.
+   * @param <T> The type of the result object.
+   * @return A promise which will be resolved with the POJO generated from the response if the request was successful.
+   */
   <T> Promise<T, FirebaseRuntimeException, Void> getValue(Class<T> clazz);
+
+  /**
+   * Sets the value in Firebase for this reference URL. This will overwrite all data that is currently stored
+   * under that location. Providing a <code>null</code> value is the equivalent of removing the data at this location.<br>
+   * <br>
+   * The promise returned will be rejected with the following two exceptions:<br>
+   * <pre>
+   * org.restonfire.exceptions.FirebaseAccessException: A {@link FirebaseRuntimeException} in the case that access to the data for this reference was denied.
+   * org.restonfire.exceptions.FirebaseRestException:   A {@link FirebaseRuntimeException} in the case that an unexpected status code was returned or the deserialization of the response into the type parameter fails.
+   * </pre>
+   * @param value The value to be written to Firebase.
+   * @param <T> The type of the parameter object.
+   * @return A promise which will resolve with the object passed in as the value if the request was successful.
+   */
   <T> Promise<T, FirebaseRuntimeException, Void> setValue(T value);
+
+  /**
+   * Updates the value in Firebase for this reference URL. Only the properties (child nodes) that
+   * are included in this value object will be overwritten. Any other properties not included in the value
+   * will remain the same.<br>
+   * <br>
+   * The promise returned will be rejected with the following two exceptions:<br>
+   * <pre>
+   * org.restonfire.exceptions.FirebaseAccessException: A {@link FirebaseRuntimeException} in the case that access to the data for this reference was denied.
+   * org.restonfire.exceptions.FirebaseRestException:   A {@link FirebaseRuntimeException} in the case that an unexpected status code was returned or the deserialization of the response into the type parameter fails.
+   * </pre>
+   * @param value The value to be written to Firebase.
+   * @param <T> The type of the parameter object.
+   * @return A promise which will resolve with the object passed in as the value if the request was successful.
+   */
   <T> Promise updateValue(T value);
-  <T> Promise<Void, FirebaseRuntimeException, Void> removeValue();
+
+  /**
+   * Removes the value for this reference URL from Firebase. This is the requivalent to setting the value
+   * to <code>null</code>.<br>
+   * <br>
+   * The promise returned will be rejected with the following two exceptions:<br>
+   * <pre>
+   * org.restonfire.exceptions.FirebaseAccessException: A {@link FirebaseRuntimeException} in the case that access to the data for this reference was denied.
+   * org.restonfire.exceptions.FirebaseRestException:   A {@link FirebaseRuntimeException} in the case that an unexpected status code was returned.
+   * </pre>
+   * @return A promise which will be resolved with a <code>null</code> value if the request was successful.
+   */
+  Promise<Void, FirebaseRuntimeException, Void> removeValue();
+
+  /**
+   * Creates a new child property under the current location. Firebase will give the child node a unique
+   * identifier returned by this .<br>
+   * <br>
+   * The promise returned will be rejected with the following two exceptions:<br>
+   * <pre>
+   * org.restonfire.exceptions.FirebaseAccessException: A {@link FirebaseRuntimeException} in the case that access to the data for this reference was denied.
+   * org.restonfire.exceptions.FirebaseRestException:   A {@link FirebaseRuntimeException} in the case that an unexpected status code was returned.
+   * </pre>
+   * @return The reference to the newly created child in Firebase.
+   *
+   * @see <a href="https://www.firebase.com/docs/android/api/#firebase_push">Firebase Android Documentation</a>
+   */
   Promise<FirebaseRestReference, FirebaseRuntimeException, Void> push();
 
+  /**
+   * Returns the reference for the root of this Firebase namespace.
+   *
+   * @return The {@link FirebaseRestReference} representing the root of this Firebase namespace.
+   */
   FirebaseRestReference getRoot();
+
+  /**
+   * Returns the reference for the parent location of this {@link FirebaseRestReference}.
+   *
+   * @return The {@link FirebaseRestReference} representing the parent location of this {@link FirebaseRestReference}.
+   */
   FirebaseRestReference getParent();
+
+  /**
+   * Returns the reference for the given child location of this {@link FirebaseRestReference}.
+   *
+   * @param path The child's name or path to the child relative to this reference.
+   * @return The {@link FirebaseRestReference} representing the child location.
+   */
   FirebaseRestReference child(String path);
 
   // TODO: Add query functions limitToFirst, limitToLast, startAt, endAt, equalTo
