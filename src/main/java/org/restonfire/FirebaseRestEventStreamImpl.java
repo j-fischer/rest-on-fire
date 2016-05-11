fe82e50d7c7514164674bbf2c4533312b5cb8a14
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-05-10 23:01:04 -0600

    Changed javadoc comment.

--- src/main/java/org/restonfire/FirebaseRestEventStreamImpl.java
+++ src/main/java/org/restonfire/FirebaseRestEventStreamImpl.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jfischer on 2016-05-05.
+ * {@link FirebaseRestEventStream} implementation.
  */
 class FirebaseRestEventStreamImpl extends FirebaseDocumentLocation implements FirebaseRestEventStream {
 
