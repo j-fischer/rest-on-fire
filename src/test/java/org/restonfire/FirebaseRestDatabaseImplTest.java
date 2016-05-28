4c74a85d4e97d45495a0587d0a52148f2b8fba78
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-05-28 17:12:50 -0600

    Added missing unit tests for FirebaseRestDatabaseImpl

--- src/test/java/org/restonfire/FirebaseRestDatabaseImplTest.java
+++ src/test/java/org/restonfire/FirebaseRestDatabaseImplTest.java
@@ -3,10 +3,12 @@ package org.restonfire;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ning.http.client.AsyncHttpClient;
+import org.jmock.Expectations;
 import org.junit.Test;
 import org.restonfire.testutils.AbstractMockTestCase;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Test class for FirebaseRestNamespaceImpl.
@@ -14,6 +16,7 @@ import static org.junit.Assert.assertEquals;
 public class FirebaseRestDatabaseImplTest extends AbstractMockTestCase {
 
   private final AsyncHttpClient asyncHttpClient = mock(AsyncHttpClient.class);
+  private final AsyncHttpClient.BoundRequestBuilder requestBuilder = mock(AsyncHttpClient.BoundRequestBuilder.class);
 
   private final Gson gson = new GsonBuilder().create();
 
@@ -27,6 +30,29 @@ public class FirebaseRestDatabaseImplTest extends AbstractMockTestCase {
   public void testGetReference() {
     FirebaseRestReference result = namespace.getReference(path);
 
+    assertNotNull(result);
     assertEquals(fbBaseUrl + PathUtil.FORWARD_SLASH + path, result.getReferenceUrl());
+  }
+
+  @Test
+  public void testGetEventStream() {
+    addExpectations(new Expectations() {{
+      oneOf(asyncHttpClient).prepareGet("https://mynamespace.firebaseio.com/foo/bar.json"); will(returnValue(requestBuilder));
+      oneOf(requestBuilder).addHeader("Accept", "text/event-stream"); will(returnValue(requestBuilder));
+      oneOf(requestBuilder).addQueryParam("auth", fbAccessToken); will(returnValue(requestBuilder));
+      oneOf(requestBuilder).setFollowRedirects(true); will(returnValue(requestBuilder));
+    }});
+
+    FirebaseRestEventStream result = namespace.getEventStream(path);
+
+    assertNotNull(result);
+    assertEquals(fbBaseUrl + PathUtil.FORWARD_SLASH + path, result.getReferenceUrl());
+  }
+
+  @Test
+  public void testGetSecurityRules() {
+    FirebaseSecurityRulesReference result = namespace.getSecurityRules();
+
+    assertNotNull(result);
   }
 }
